1b44d19011b1ba41c2dde354bba4d4f5864b70c9
Mohana46 <jerseyno.46@gmail.com>
2026-04-20 15:18:40 +0530

    Register request dto

--- Backend/src/main/java/com/authentication/dto/request/RegisterRequest.java
+++ Backend/src/main/java/com/authentication/dto/request/RegisterRequest.java
@@ -1,5 +1,24 @@
 package com.authentication.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
 
+    @NotBlank
+    @Pattern(
+        regexp = "^[A-Z][a-zA-Z]{3,}\\s[A-Za-z]{1,}$",
+        message = "Full name must be like 'John D' (First name capital, min 4 letters, last name min 1 letter)"
+    )
+    private String fullName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
